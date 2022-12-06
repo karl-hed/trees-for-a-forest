d2d8bf3f5bd6ed81811c3477202a1e2a42797b9e
enayatullah azizzada <e_azizzada@yahoo.com>
2022-12-06 17:37:29 -0500

    changes to controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,41 +3,43 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+
   end
 
   def show
   end
 
-  def new
-    @event = Event.new
-  end
+  # def new
+  #   @event = Event.new
+  #   @organization = Organization.find(params[:organization_id])
+  # end
 
-  def create
-    @event = Event.new(event_params)
-    @event.organization = @organization
-    @event.user = current_user
-    if @event.save
-      redirect_to organization_path(@organization), notice: "event was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @organization = Organization.find(params[:organization_id])
+  #   @event = Event.new(event_params)
+  #   @event.organization = @organization
+  #   if @event.save
+  #     redirect_to organization_path(@organization), notice: "event was successfully created."
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-    if @event.update(event_params)
-      redirect_to @event, notice: "event was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @event.update(event_params)
+  #     redirect_to @event, notice: "event was successfully updated."
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
 
-  def destroy
-    @event.destroy
-    redirect_to root_path, notice: "event was successfully destroyed."
-  end
+  # def destroy
+  #   @event.destroy
+  #   redirect_to root_path, notice: "event was successfully destroyed."
+  # end
 
   private
 
