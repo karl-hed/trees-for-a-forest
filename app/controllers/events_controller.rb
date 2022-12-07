bfe912d2db89001b6ea538151d3f9de73af0e9b8
Karl Hedayati <k.hedayati.10@gmail.com>
2022-12-07 12:48:22 -0500

    _event.html.erb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,10 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :set_event, only: %i[show edit update destroy]
 
   def index
     @events = Event.all
-
+    @booking = Booking.new
+    @bookings = Booking.where(event: @event)
   end
 
   def show
