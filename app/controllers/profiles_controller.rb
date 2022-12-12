0f0afb6a9e0f5de78ca0ee99db864dc7e734ff20
Karl Hedayati <k.hedayati.10@gmail.com>
2022-12-12 15:38:24 -0500

    maps loads also

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,10 +3,9 @@ class ProfilesController < ApplicationController
     @user = User.find(params[:id])
     @events_array = []
     @user.bookings.each do |booking|
-      @events_array << booking.event
-      # if booking.event.date < Date.today && booking.event != nil
-        # @events_array << booking.event
-      # end
+      if booking.event.date < Date.today && booking.event != nil
+        @events_array << booking.event
+      end
     end
     @events_array
   end
--- app/javascript/controllers/event_map_controller.js
+++ app/javascript/controllers/event_map_controller.js
@@ -8,9 +8,7 @@ export default class extends Controller {
   }
 
   connect() {
-    // console.log("connect", this.apiKeyValue);
     mapboxgl.accessToken = this.apiKeyValue
-    // console.log(this.apiKeyValue);
 
     this.map = new mapboxgl.Map({
       container: this.element,
@@ -24,26 +22,15 @@ export default class extends Controller {
   }
 
   #fitMapToMarkers() {
-    // console.log('fitMapToMarkers', this.eventValue.longitude, this.eventValue.latitude)
     const bounds = new mapboxgl.LngLatBounds()
 
     this.eventsValue.forEach((event) => {
       bounds.extend([event.longitude, event.latitude ])
     })
     this.map.fitBounds(bounds, {padding: 50})
-
-    // bounds.extend([this.eventsValue.longitude, this.eventsValue.latitude ])
-    // console.log(bounds)
-    // this.map.fitBounds(bounds, {padding: 50, zoom: 14})
   }
 
   #addMarkersToMap() {
-    // console.log("addMarkersToMap");
-    // const popup = new mapboxgl.Popup().setHTML(this.eventValue.name)
-    // new mapboxgl.Marker()
-    //   .setLngLat([ this.eventValue.longitude, this.eventValue.latitude ])
-    //   .setPopup(popup)
-    //   .addTo(this.map);
     this.eventsValue.forEach((event) => {
       console.log(event.name);
       const popup = new mapboxgl.Popup().setHTML(event.name)
