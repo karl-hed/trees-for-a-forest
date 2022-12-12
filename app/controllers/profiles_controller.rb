27ba600dcf9b659dd073d63f59648f3f57eb89fe
Karl Hedayati <k.hedayati.10@gmail.com>
2022-12-12 16:49:47 -0500

    modified partials for image map

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,19 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @events_array = []
+    @past_events = []
     @user.bookings.each do |booking|
       if booking.event.date < Date.today && booking.event != nil
-        @events_array << booking.event
+        @past_events << booking.event
       end
     end
-    @events = @events_array.map do |event|
+    @markers = @past_events.map do |event|
       {
         lat: event.latitude,
         lng: event.longitude,
         info_window: render_to_string(partial: "info_window", locals: { event: event }),
-        image_url: helpers.asset_url("simple_logo")
+        image_url: helpers.asset_url("simple_logo.png")
       }
     end
-    @events_array
-    @events
   end
 end
--- app/javascript/controllers/event_map_controller.js
+++ app/javascript/controllers/event_map_controller.js
@@ -8,6 +8,7 @@ export default class extends Controller {
   }
 
   connect() {
+    console.log(this.eventsValue)
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
@@ -24,28 +25,29 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
 
-    this.eventsValue.forEach((event) => {
-      bounds.extend([ event.longitude, event.latitude ])
+    this.eventsValue.forEach((marker) => {
+      bounds.extend([ marker.lng, marker.lat ])
     })
     this.map.fitBounds(bounds, {padding: 50})
   }
 
   #addMarkersToMap() {
-    this.eventsValue.forEach((event) => {
-      console.log(event.name);
+    this.eventsValue.forEach((marker) => {
+      // console.log(marker.name);
 
       // Create a HTML element for your custom marker
       const customMarker = document.createElement("div")
       customMarker.className = "marker"
-      customMarker.style.backgroundImage = `url('${event.image_url}')`
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`
       customMarker.style.backgroundSize = "contain"
       customMarker.style.width = "25px"
       customMarker.style.height = "25px"
 
-      const popup = new mapboxgl.Popup().setHTML(event.name)
+      // const popup = new mapboxgl.Popup().setHTML(marker.name)
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       new mapboxgl.Marker()
-        // .setLngLat([ event.longitude, event.latitude ])
-        .setLngLat([ event.longitude, event.latitude ])
+        // .setLngLat([ marker.longitude, marker.latitude ])
+        .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(this.map);
     })
