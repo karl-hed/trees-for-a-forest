393d4330ab8048123361338e032ae0334d777d82
Karl Hedayati <k.hedayati.10@gmail.com>
2022-12-12 12:05:42 -0500

    removed pgsearch

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,15 +2,4 @@ class Event < ApplicationRecord
   belongs_to :organization
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
-  # include PgSearch::Model
-  # multisearchable against: [:name, :description, :region]
-
-  # pg_search_scope :search_by_name_and_description_and_region,
-  #   against: [:name, :description, :region],
-  #   associated_against: {
-  #     organization: [:name]
-  #   },
-  #   using: {
-  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  #   }
 end
