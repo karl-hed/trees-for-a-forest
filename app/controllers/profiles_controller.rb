6b12ac3426e9cf1a38f5b65e6d5f2f476e1841e7
Karl Hedayati <k.hedayati.10@gmail.com>
2022-12-12 21:55:08 -0500

    show.html.erb organization check if user_signed_in

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,18 @@
 class ProfilesController < ApplicationController
+  before_action :logged_in_user
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @past_events = []
