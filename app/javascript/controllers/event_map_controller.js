47c0f9143d9a79bb6d69e10c8758f18c1f72be6d
Enayatullah Azizzada <95454760+Azizzada@users.noreply.github.com>
2023-06-07 19:42:42 -0400

    mapbox changes

--- app/javascript/controllers/event_map_controller.js
+++ app/javascript/controllers/event_map_controller.js
@@ -1,32 +1,37 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 // JS CONTROLLER FOR SHOW(EACH EVENT) PAGE
 export default class extends Controller {
   static values = {
     apiKey: String,
-    event: Object
-  }
+    event: Object,
+  };
   connect() {
-    mapboxgl.accessToken = this.apiKeyValue
+    mapboxgl.accessToken = this.apiKeyValue;
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v11"
-    })
-    this.#addMarkersToMap()
+      style: "mapbox://styles/enayatullahazizzada/climcc0ks018o01p7apppcoy0",
+      scrollZoom: false,
+    });
+    this.#addMarkersToMap();
     setTimeout(() => {
-      this.#fitMapToMarkers()
-    }, 300)
+      this.#fitMapToMarkers();
+    }, 300);
   }
   #fitMapToMarkers() {
-    console.log('fitMapToMarkers', this.eventValue.longitude, this.eventValue.latitude)
-    const bounds = new mapboxgl.LngLatBounds()
-    bounds.extend([this.eventValue.longitude, this.eventValue.latitude ])
-    console.log(bounds)
-    this.map.fitBounds(bounds, {padding: 50, zoom: 14})
+    console.log(
+      "fitMapToMarkers",
+      this.eventValue.longitude,
+      this.eventValue.latitude
+    );
+    const bounds = new mapboxgl.LngLatBounds();
+    bounds.extend([this.eventValue.longitude, this.eventValue.latitude]);
+    console.log(bounds);
+    this.map.fitBounds(bounds, { padding: 50, zoom: 14 });
   }
   #addMarkersToMap() {
-    const popup = new mapboxgl.Popup().setHTML(this.eventValue.name)
+    const popup = new mapboxgl.Popup().setHTML(this.eventValue.name);
     new mapboxgl.Marker()
-      .setLngLat([ this.eventValue.longitude, this.eventValue.latitude ])
+      .setLngLat([this.eventValue.longitude, this.eventValue.latitude])
       .setPopup(popup)
       .addTo(this.map);
   }
