810550e0463d0952b8987fbf523a76075008c485
Enayatullah Azizzada <95454760+Azizzada@users.noreply.github.com>
2023-06-07 19:46:56 -0400

    mapbox changes2

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,41 +1,42 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // JS CONTROLLER FOR INDEX/EVENTS PAGE
 export default class extends Controller {
   static values = {
     apiKey: String,
-    events: Array
-  }
+    events: Array,
+  };
 
   connect() {
-    mapboxgl.accessToken = this.apiKeyValue
+    mapboxgl.accessToken = this.apiKeyValue;
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v11"
-    })
-    this.#addMarkersToMap()
+      style: "mapbox://styles/enayatullahazizzada/climcc0ks018o01p7apppcoy0",
+      scrollZoom: false,
+    });
+    this.#addMarkersToMap();
 
     setTimeout(() => {
-      this.#fitMapToMarkers()
-    }, 300)
+      this.#fitMapToMarkers();
+    }, 300);
   }
 
   #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
+    const bounds = new mapboxgl.LngLatBounds();
     this.eventsValue.forEach((event) => {
-      bounds.extend([event.longitude, event.latitude ])
-    })
-    this.map.fitBounds(bounds, {padding: 50})
+      bounds.extend([event.longitude, event.latitude]);
+    });
+    this.map.fitBounds(bounds, { padding: 50 });
   }
 
   #addMarkersToMap() {
     this.eventsValue.forEach((event) => {
-      const popup = new mapboxgl.Popup().setHTML(event.name)
+      const popup = new mapboxgl.Popup().setHTML(event.name);
       new mapboxgl.Marker()
-        .setLngLat([ event.longitude, event.latitude ])
+        .setLngLat([event.longitude, event.latitude])
         .setPopup(popup)
         .addTo(this.map);
-    })
+    });
   }
 }
